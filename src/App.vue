30fb4378f9f4202aa8d4b970a3e5372e357698d6
AlloyceOloo <alloyceoloo5@gmail.com>
2024-03-07 09:40:34 +0300

    Tested API functionality

--- src/App.vue
+++ src/App.vue
@@ -1,15 +1,48 @@
 <template>
-  <img alt="Vue logo" src="./assets/logo.png">
-  <HelloWorld msg="Welcome to Your Vue.js App"/>
+  <div id="app" class="container">
+    <div class="row">
+      <div class="col-md-6 offset-md-3 py-5">
+        <h1>Generate a thumbnail of a website</h1>
+		<img :src="thumbnailUrl"/>
+        <form v-on:submit.prevent="makeWebsiteThumbnail">
+          <div class="form-group">
+            <input v-model="websiteUrl" type="text" id="website-input" placeholder="Enter a website" class="form-control">
+          </div>
+          <div class="form-group">
+            <button class="btn btn-primary">Generate!</button>
+          </div>
+        </form>
+      </div>
+    </div>
+  </div>
 </template>
 
 <script>
-import HelloWorld from './components/HelloWorld.vue'
+import axios from 'axios';
 
 export default {
   name: 'App',
-  components: {
-    HelloWorld
+
+  data() { return {
+    websiteUrl: '',
+    thumbnailUrl: ''
+  }  },
+
+  methods: {
+    makeWebsiteThumbnail() {
+      axios.post("https://shot.screenshotapi.net/screenshot?token={token}&url={url}", {
+	token: "7HE9WFB-HNP4EKT-QWPY642-JQXH4FT",
+	url: this.websiteUrl,
+	width: 1920,
+	height: 1000,
+	output: 'json',
+	thumbnail_width: 300
+	})
+	.then((response) => {
+		this.thumbnailUrl = response.data.screenshot;
+	})
+	
+    }
   }
 }
 </script>
